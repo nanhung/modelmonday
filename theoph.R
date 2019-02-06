8e245d8a3eebc7eb56681fb668b744d6a89ef924
Nan-Hung Hsieh <nhsieh@cvm.tamu.edu>
2019-02-06 11:18:46 -0600

    add example

--- theoph.R
+++ theoph.R
@@ -1,6 +1,23 @@
 # http://wiekvoet.blogspot.com/2015/04/hierarchical-two-compartimental-pk-model.html
 rm(list=ls())
 
+coplot(conc ~ Time | Subject, data = Theoph, show.given = FALSE)
+Theoph.4 <- subset(Theoph, Subject == 4)
+fm1 <- nls(conc ~ SSfol(Dose, Time, lKe, lKa, lCl),
+           data = Theoph.4)
+summary(fm1)
+plot(conc ~ Time, data = Theoph.4,
+     xlab = "Time since drug administration (hr)",
+     ylab = "Theophylline concentration (mg/L)",
+     main = "Observed concentrations and fitted model",
+     sub  = "Theophylline data - Subject 4 only",
+     las = 1, col = 4)
+xvals <- seq(0, par("usr")[2], length.out = 55)
+lines(xvals, predict(fm1, newdata = list(Time = xvals)),
+      col = 4)
+
+
+
 library(lattice)
 library(latticeExtra)
 library(tidyverse)
